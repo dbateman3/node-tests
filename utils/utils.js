079dbc07fe61017f71880a0e3f07e2cd626b535f
Dan Bateman <dbateman0319@starkstate.net>
2017-12-14 23:41:02 -0500

    Using expect library

--- utils/utils.js
+++ utils/utils.js
@@ -6,3 +6,10 @@ module.exports.add = function(a, b) {
 module.exports.square = function(x) {
 	return x * x ;
 };
+
+module.exports.setName = function(user, fullName) {
+	let name = fullName.split(' ');
+	user.firstName = name[0];
+	user.lastName = name[1];
+	return user;
+};
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,17 +1,49 @@
+const expect = require('expect');
 const utils = require('./utils');
 //test case runs code checks for passed/failed behavior driven development
 //it is a function from mocha, no imports
 //2 arguments, string description
 it('should add two numbers', function() {
 	let res = utils.add(33, 11);
-	if (res != 44) {
-		throw new Error('Incorrect answer');
-	}
+	//assertion
+	expect(res).toBeA('number').toBe(44);
+	// if (res != 44) {
+	//	throw new Error('Incorrect answer')	
+	//}
 });
 
 it('should square a number', function() {
 	let res = utils.square(2);
-	if (res != 4) {
-		throw new Error(`Incorrect answer, expected 2, got ${res}`);
-	}
+	
+	expect(res).toBeA('number').toBe(4);
+
+	//if (res != 4) {
+	//	throw new Error(`Incorrect answer, expected 2, got ${res}`);
+	//}
+});
+
+it('should expect some values', function() {
+	//expect(12).toNotBe(11);
+	//tobe does not work on objects and arrays
+	//expect({name: 'Dan'}).toEqual({name: 'Dan'});
+	/*check if a value is in an array and object
+	expect({
+		name: 'Dan',
+		age: 25
+	}).toInclude({
+		age: 25
+	});
+	expect([2, 3, 4]).toInclude(2);
+	*/
+});
+
+it('should set first and last name properties', function() {
+	let user = {
+		age: 25,
+	};
+	let res = utils.setName(user, 'Dan Bateman');
+	expect(res).toInclude({
+		firstName: 'Dan',
+		lastName: 'Bateman'
+	});
 });
